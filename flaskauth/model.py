2610b80392c6da8ca4ad0beff80a1026cb108b1b
Chithra <92291415+chithra-m@users.noreply.github.com>
2022-04-01 15:19:57 +0530

    Added password validation

--- flaskauth/model.py
+++ flaskauth/model.py
@@ -1,5 +1,5 @@
 from flask_login import UserMixin
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+# from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flaskauth import login_manager, db, app
 
 @login_manager.user_loader
@@ -13,16 +13,16 @@ class Users(db.Model, UserMixin):
     email = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(50), nullable=False)
 
-    def get_reset_token(self, expires_sec=1800):
-        s = Serializer(app.config['SECRET_KEY'], expires_sec)
-        return s.dumps({'user_id': self.id}).decode('utf-8')
-        # Creates token with 30 minutes expiration
+    # def get_reset_token(self, expires_sec=1800):
+    #     s = Serializer(app.config['SECRET_KEY'], expires_sec)
+    #     return s.dumps({'user_id': self.id}).decode('utf-8')
+    #     # Creates token with 30 minutes expiration
         
-    @staticmethod
-    def verify_reset_token(token):
-        s = Serializer(app.config['SECRET_KEY'])
-        try:    
-            user_id = s.loads(token)['user_id']
-        except:
-            return None
-        return Users.query.get(user_id)
+    # @staticmethod
+    # def verify_reset_token(token):
+    #     s = Serializer(app.config['SECRET_KEY'])
+    #     try:    
+    #         user_id = s.loads(token)['user_id']
+    #     except:
+    #         return None
+    #     return Users.query.get(user_id)
